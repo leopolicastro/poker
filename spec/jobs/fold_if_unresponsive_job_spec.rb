9bb478d78dd7e2cb898e37ffff4dc6dbacd31694
leopolicastro <43428385+leopolicastro@users.noreply.github.com>
2025-05-18 16:51:01 -0400

    test: 💍 fix

--- spec/jobs/fold_if_unresponsive_job_spec.rb
+++ spec/jobs/fold_if_unresponsive_job_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe FoldIfUnresponsiveJob, type: :job do
     it "folds the player if they are unresponsive" do
       expect(player.folded?).to be_falsey
       described_class.perform_now(
-        round_id: round.id,
-        player_id: player.id,
+        round:,
+        player:,
         current_bets_count: 2
       )
       expect(player.reload.folded?).to be_truthy
